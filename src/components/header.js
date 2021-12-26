5f8e5ccd1563caeb1dd6acc64ba413084f67f72f
tatsumara <sutakumu@gmail.com>
2021-12-26 13:27:20 +0100

    Added clock

--- src/components/header.js
+++ src/components/header.js
@@ -5,4 +5,20 @@ header.innerHTML = /*html*/ `
 	</div>
 	<a href="https://discord.gg/accela">discord</a>
 	<a href="https://github.com/tatsumara">github</a>
+	<div id="time"></div>
 `
+function updateClock() {
+	const now = new Date();
+	let hours = now.getHours();
+	let minutes = now.getMinutes();
+	let seconds = now.getSeconds();
+
+	hours = hours < 10 ? '0' + hours : hours;
+	minutes = minutes < 10 ? '0' + minutes : minutes;
+	seconds = seconds < 10 ? '0' + seconds : seconds;
+
+	const time = document.getElementById('time');
+	time.innerHTML = `${hours}:${minutes}:${seconds}`
+}
+updateClock();
+setInterval(updateClock, 1000);
