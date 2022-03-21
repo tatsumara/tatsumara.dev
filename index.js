b379ecec32f973a2af5ccd36c33e5bda7194826c
tatsumara <mara@tatsumara.tk>
2022-03-21 17:11:44 +0100

    feat: use morgan for logging

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
+const morgan = require('morgan');
 const express = require('express');
 const app = express();
 
+app.use(morgan('[:date[web]] :remote-addr :method :url :status'))
 app.use(express.static('static'))
 
 app.listen(80);
+console.log('Listening on port 80.')
