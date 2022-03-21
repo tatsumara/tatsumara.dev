ba8f3d433f52eb8304d3550f8351014a707ed0c8
tatsumara <mara@tatsumara.tk>
2022-03-21 17:28:03 +0100

    fix: fix ip's behind proxy

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ const morgan = require('morgan');
 const express = require('express');
 const app = express();
 
-app.use(morgan('[:date[web]] :remote-addr :method :url :status'))
-app.use(express.static('static'))
+app.use(morgan('[:date[web]] :remote-addr :method :url :status'));
+app.enable('trust proxy');
+app.use(express.static('static'));
 
 app.listen(80);
-console.log('Listening on port 80.')
+console.log('Listening on port 80.');
