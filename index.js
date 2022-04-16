e2aa64fd02bb9030cab82404555ce60af248758c
tatsumara <mara@tatsumara.dev>
2022-04-16 23:49:12 +0200

    feat: add 404 page

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const app = express();
 app.use(morgan('[:date[web]] :remote-addr :method :url :status'));
 app.set('trust proxy', true);
 app.use(express.static('static'));
+app.use((req, res, next) => {
+	res.status(404).sendFile(__dirname + '/static/404.html');
+})
 
 app.listen(80);
 console.log('Listening on port 80.');
