0b0933fdfa5ca2aaf6d85a36c0f772aaac75b44d
tatsumara <mara@tatsumara.dev>
2022-06-13 20:23:03 +0200

    feat: add top3 songs endpoint

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
 const morgan = require('morgan');
 const express = require('express');
 const app = express();
+const fetch = require('isomorphic-unfetch');
+const spot = require('spotify-url-info')(fetch);
 
 app.use(morgan('[:date[web]] :remote-addr :method :url :status'));
 app.set('trust proxy', true);
 app.use(express.static('static'));
+
+app.get('/top3', async (req, res) => {
+	let data = await spot.getTracks('https://open.spotify.com/playlist/37i9dQZF1EpwA0Eb3mMXw4');
+	data = data.sort(() => 0.5 - Math.random()).slice(0, 3).map(song => { return song.external_urls.spotify });
+	res.send(data);
+});
+
 app.use((req, res, next) => {
 	res.status(404).sendFile(__dirname + '/static/404.html');
-})
+});
+
 
 app.listen(80);
 console.log('Listening on port 80.');
